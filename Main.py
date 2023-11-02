b89351825327611dbed856c07bcb53f25597e5cf
amrati123 <96200775+amrati123@users.noreply.github.com>
2023-11-02 18:42:23 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,10 @@
 import customtkinter
-
 root = customtkinter.CTk()
 root.title("TitanoLauncher")
 root.geometry("500x500")
-def launch()
-     print("l")
-          
-          
-Btu = customtkinter.button(window = root, command = launch, text = "button")
+def launch():
+    print("l")
 
-     root.mainloop()
+Btu = customtkinter.CTkButton(master=root, text="button", command=launch())
+Btu.pack()
+root.mainloop()
